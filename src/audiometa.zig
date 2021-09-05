894266f7a2afdb356898fed017c410ddc8949a30
Ryan Liptak <squeek502@hotmail.com>
2021-09-05 00:00:53 -0700

    Add missing imports to audiometa.zig

--- src/audiometa.zig
+++ src/audiometa.zig
@@ -6,6 +6,8 @@ pub const id3v2 = @import("id3v2.zig");
 pub const latin1 = @import("latin1.zig");
 pub const metadata = @import("metadata.zig");
 pub const synchsafe = @import("synchsafe.zig");
+pub const unsynch = @import("unsynch.zig");
+pub const buffered_stream_source = @import("buffered_stream_source.zig");
 pub const util = @import("util.zig");
 
 test "" {
