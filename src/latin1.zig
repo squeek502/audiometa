acde5eb51b347e681052ca77468dbbbb905faf4d
Ryan Liptak <squeek502@hotmail.com>
2021-12-30 02:54:34 -0800

    Add functions for utf8 -> latin1 conversion

--- src/latin1.zig
+++ src/latin1.zig
@@ -49,18 +49,62 @@ pub fn isUtf8AllLatin1(utf8_text: []const u8) bool {
     return true;
 }
 
-test "latin1 to utf8" {
-    var buf: [512]u8 = undefined;
-    const utf8 = latin1ToUtf8("a\xE0b\xE6c\xEFd", &buf);
+pub fn utf8ToLatin1Alloc(allocator: Allocator, utf8_text: []const u8) ![]u8 {
+    assert(isUtf8AllLatin1(utf8_text));
 
-    try std.testing.expectEqualSlices(u8, "aàbæcïd", utf8);
+    var buffer = try std.ArrayList(u8).initCapacity(allocator, utf8_text.len);
+    errdefer buffer.deinit();
+
+    var utf8_it = std.unicode.Utf8Iterator{ .bytes = utf8_text, .i = 0 };
+    while (utf8_it.nextCodepoint()) |codepoint| {
+        // this cast is guaranteed to work since we know the UTF-8 is made up
+        // of all Latin-1 characters
+        try buffer.append(@intCast(u8, codepoint));
+    }
+
+    return buffer.toOwnedSlice();
 }
 
-test "latin1 to utf8 alloc" {
-    const utf8 = try latin1ToUtf8Alloc(std.testing.allocator, "a\xE0b\xE6c\xEFd");
+/// buf must be at least 1/2 the size of utf8_text
+pub fn utf8ToLatin1(utf8_text: []const u8, buf: []u8) []u8 {
+    assert(isUtf8AllLatin1(utf8_text));
+    assert(buf.len >= utf8_text.len / 2);
+
+    var i: usize = 0;
+    var utf8_it = std.unicode.Utf8Iterator{ .bytes = utf8_text, .i = 0 };
+    while (utf8_it.nextCodepoint()) |codepoint| {
+        // this cast is guaranteed to work since we know the UTF-8 is made up
+        // of all Latin-1 characters
+        buf[i] = @intCast(u8, codepoint);
+        i += 1;
+    }
+
+    return buf[0..i];
+}
+
+test "latin1 to utf8 and back" {
+    var utf8_buf: [512]u8 = undefined;
+    var latin1_buf: [512]u8 = undefined;
+
+    const latin1 = "a\xE0b\xE6c\xEFd";
+    const utf8 = latin1ToUtf8(latin1, &utf8_buf);
+    try std.testing.expectEqualSlices(u8, "aàbæcïd", utf8);
+
+    const latin1_converted = utf8ToLatin1(utf8, &latin1_buf);
+    try std.testing.expectEqualSlices(u8, latin1, latin1_converted);
+}
+
+test "latin1 to utf8 alloc and back" {
+    const latin1 = "a\xE0b\xE6c\xEFd";
+    const utf8 = try latin1ToUtf8Alloc(std.testing.allocator, latin1);
     defer std.testing.allocator.free(utf8);
 
     try std.testing.expectEqualSlices(u8, "aàbæcïd", utf8);
+
+    const latin1_converted = try utf8ToLatin1Alloc(std.testing.allocator, utf8);
+    defer std.testing.allocator.free(latin1_converted);
+
+    try std.testing.expectEqualSlices(u8, latin1, latin1_converted);
 }
 
 test "is utf8 all latin1" {
