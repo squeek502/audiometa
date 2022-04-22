6bf4f67dcf180fabba5e43877a485c359000dd8f
Ryan Liptak <squeek502@hotmail.com>
2022-04-22 04:07:21 -0700

    Refactor and slightly improve mp4 parsing
    - Rename/move readX functions to X.read instead (personal preference)
    - Reduce magic number usage, move everything to named constants
    - Instead of returning an error on data types that we don't currently handle, we now skip them instead. This allows us to fully read any mp4 metadata while extracting the stuff we know how to read.

--- src/mp4.zig
+++ src/mp4.zig
@@ -7,14 +7,16 @@ const Metadata = @import("metadata.zig").Metadata;
 const FullAtomHeader = struct {
     version: u8,
     flags: u24,
-};
 
-fn readFullAtomHeader(reader: anytype) !FullAtomHeader {
-    return FullAtomHeader{
-        .version = try reader.readByte(),
-        .flags = try reader.readIntBig(u24),
-    };
-}
+    pub const len = 4;
+
+    pub fn read(reader: anytype) !FullAtomHeader {
+        return FullAtomHeader{
+            .version = try reader.readByte(),
+            .flags = try reader.readIntBig(u24),
+        };
+    }
+};
 
 /// Every atom in a MP4 file has this fixed-size header
 const AtomHeader = struct {
@@ -22,93 +24,153 @@ const AtomHeader = struct {
     size: u32,
     /// the name or type
     name: [4]u8,
+
+    pub const len = 8;
+
+    pub fn read(reader: anytype, seekable_stream: anytype) !AtomHeader {
+        var header: AtomHeader = undefined;
+        header.size = switch (try reader.readIntBig(u32)) {
+            0 => blk: {
+                // a size of 0 means the atom extends to end of file
+                const remaining = (try seekable_stream.getEndPos()) - (try seekable_stream.getPos());
+                break :blk @intCast(u32, remaining);
+            },
+            1 => {
+                // a size of 1 means the atom header has an extended size field
+                // TODO: implement this if relevant ?
+                return error.UnimplementedExtendedSize;
+            },
+            else => |n| n,
+        };
+
+        if (header.size < AtomHeader.len) {
+            return error.AtomSizeTooSmall;
+        }
+
+        const remaining = (try seekable_stream.getEndPos()) - (try seekable_stream.getPos());
+        if (header.sizeExcludingHeader() >= remaining) {
+            return error.EndOfStream;
+        }
+
+        _ = try reader.readAll(&header.name);
+
+        return header;
+    }
+
+    pub fn sizeExcludingHeader(self: AtomHeader) u32 {
+        return self.size - AtomHeader.len;
+    }
 };
-
-fn readAtomHeader(reader: anytype, seekable_stream: anytype) !AtomHeader {
-    const size = switch (try reader.readIntBig(u32)) {
-        0 => blk: {
-            // a size of 0 means the atom extends to end of file
-
-            const remaining = (try seekable_stream.getEndPos()) - (try seekable_stream.getPos());
-
-            break :blk @intCast(u32, remaining);
-        },
-        1 => {
-            // a size of 1 means the atom header has an extended size field
-            // TODO: implement this if relevant ?
-
-            return error.UnimplementedExtendedSize;
-        },
-        else => |n| n,
-    };
-
-    if (size < 8) {
-        return error.AtomSizeTooSmall;
-    }
-
-    const remaining = (try seekable_stream.getEndPos()) - (try seekable_stream.getPos());
-    if (size - 8 >= remaining) {
-        return error.EndOfStream;
-    }
-
-    var name: [4]u8 = undefined;
-    _ = try reader.readAll(&name);
-
-    return AtomHeader{
-        .size = size,
-        .name = name,
-    };
-}
 
 /// Generic data atom
 ///
-/// Its value should be interpreted based on the type indicator and locale indicator.
-/// For our purposes it's probably enough to treat them as UTF-8 strings.
-///
 /// See https://developer.apple.com/library/archive/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW27
 const DataAtom = struct {
-    type_indicator: u32,
-    locale_indicator: u32,
-    value: []u8,
+    header: AtomHeader,
+    indicators: Indicators,
 
-    /// Returns the first byte of the "type indicator" field.
-    fn getType(self: DataAtom) u8 {
-        return @intCast(u8, (self.type_indicator & 0xFF000000) >> 24);
+    pub fn read(reader: anytype, seekable_stream: anytype) !DataAtom {
+        var data_atom: DataAtom = undefined;
+
+        data_atom.header = try AtomHeader.read(reader, seekable_stream);
+        if (!std.mem.eql(u8, "data", &data_atom.header.name)) {
+            return error.InvalidDataAtom;
+        }
+
+        if (data_atom.header.sizeExcludingHeader() < Indicators.len) {
+            return error.DataAtomSizeTooSmall;
+        }
+
+        data_atom.indicators = Indicators{
+            .type_indicator = try reader.readIntBig(u32),
+            .locale_indicator = try reader.readIntBig(u32),
+        };
+
+        return data_atom;
     }
 
-    /// Returns the well-known type of the value. Only valid if the type byte (as returned in `getType`) is 0.
-    ///
-    /// See https://developer.apple.com/library/archive/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW34
-    /// for a list of well-known types.
-    fn getWellKnownType(self: DataAtom) u24 {
-        return @intCast(u24, self.type_indicator & 0x00FFFFFF);
+    pub const Indicators = struct {
+        type_indicator: u32,
+        locale_indicator: u32,
+
+        pub const len = 8;
+
+        /// Returns the first byte of the "type indicator" field.
+        fn getType(self: Indicators) Type {
+            return @intToEnum(Type, (self.type_indicator & 0xFF000000) >> 24);
+        }
+
+        pub const Type = enum(u8) {
+            well_known = 0,
+            // all non-zero type bytes are considered 'reserved'
+            _,
+        };
+
+        /// Returns the well-known type of the value, or null if the type indicator is reserved instead of well-known.
+        ///
+        /// See https://developer.apple.com/library/archive/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW34
+        /// for a list of well-known types.
+        fn getWellKnownType(self: Indicators) ?WellKnownType {
+            switch (self.getType()) {
+                .well_known => {
+                    const well_known_type = @intCast(u24, self.type_indicator & 0x00FFFFFF);
+                    return @intToEnum(WellKnownType, well_known_type);
+                },
+                else => return null,
+            }
+        }
+
+        /// From https://developer.apple.com/library/archive/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW34
+        pub const WellKnownType = enum(u24) {
+            reserved = 0,
+            utf8 = 1,
+            utf16_be = 2,
+            s_jis = 3,
+            utf8_sort = 4,
+            utf16_sort = 5,
+            jpeg = 13,
+            png = 14,
+            be_signed_integer = 21,
+            be_unsigned_integer = 22,
+            be_float32 = 23,
+            be_float64 = 24,
+            bmp = 27,
+            atom = 28,
+            signed_byte = 65,
+            be_16bit_signed_integer = 66,
+            be_32bit_signed_integer = 67,
+            be_point_f32 = 70,
+            be_dimensions_f32 = 71,
+            be_rect_f32 = 72,
+            be_64bit_signed_integer = 74,
+            unsigned_byte = 75,
+            be_16bit_unsigned_integer = 76,
+            be_32bit_unsigned_integer = 77,
+            be_64bit_unsigned_integer = 78,
+            affine_transform_f64 = 79,
+            _,
+        };
+    };
+
+    pub fn dataSize(self: DataAtom) u32 {
+        return self.header.sizeExcludingHeader() - Indicators.len;
+    }
+
+    pub fn readValueAsBytes(self: DataAtom, allocator: Allocator, reader: anytype) ![]u8 {
+        const data_size = self.dataSize();
+        var value = try allocator.alloc(u8, data_size);
+        errdefer allocator.free(value);
+
+        try reader.readNoEof(value);
+
+        return value;
+    }
+
+    pub fn skipValue(self: DataAtom, seekable_stream: anytype) !void {
+        const data_size = self.dataSize();
+        try seekable_stream.seekBy(data_size);
     }
 };
-
-fn readDataAtom(allocator: Allocator, reader: anytype, seekable_stream: anytype) !DataAtom {
-    const atom_header = try readAtomHeader(reader, seekable_stream);
-    if (!std.mem.eql(u8, "data", &atom_header.name)) return error.InvalidDataAtom;
-
-    if (atom_header.size < 16) {
-        return error.DataAtomSizeTooSmall;
-    }
-
-    // -8 for the atom header size
-    // -8 for the data atom "metadata" (type indicator and locale indicator)
-    const data_size = atom_header.size - 8 - 8;
-
-    var res = DataAtom{
-        .type_indicator = try reader.readIntBig(u32),
-        .locale_indicator = try reader.readIntBig(u32),
-        .value = try allocator.alloc(u8, data_size),
-    };
-    errdefer allocator.free(res.value);
-
-    const n = try reader.readAll(res.value);
-    if (n < data_size) return error.EndOfStream;
-
-    return res;
-}
 
 const MetadataAtom = struct {
     name: []const u8,
@@ -178,7 +240,7 @@ pub fn read(allocator: Allocator, reader: anytype, seekable_stream: anytype) !Me
     var ilst_read: usize = 0;
 
     while (true) {
-        const atom_header = readAtomHeader(reader, seekable_stream) catch |err| switch (err) {
+        const atom_header = AtomHeader.read(reader, seekable_stream) catch |err| switch (err) {
             error.EndOfStream => if (metadata.map.entries.items.len > 0) return metadata else return err,
             else => return err,
         };
@@ -194,10 +256,10 @@ pub fn read(allocator: Allocator, reader: anytype, seekable_stream: anytype) !Me
             },
             .in_udta => if (std.mem.eql(u8, "meta", &atom_header.name)) {
                 // The full atom header doesn't interest us but it has to be read.
-                _ = try readFullAtomHeader(reader);
+                _ = try FullAtomHeader.read(reader);
 
                 // The "meta" atom started at the current stream position minus the standard and full atom header.
-                metadata.start_offset = (try seekable_stream.getPos()) - 8 - 4;
+                metadata.start_offset = (try seekable_stream.getPos()) - AtomHeader.len - FullAtomHeader.len;
                 metadata.end_offset = metadata.start_offset + atom_header.size;
 
                 state = .in_meta;
@@ -205,7 +267,7 @@ pub fn read(allocator: Allocator, reader: anytype, seekable_stream: anytype) !Me
             },
             .in_meta => if (std.mem.eql(u8, "ilst", &atom_header.name)) {
                 // Used when handling the in_ilst state to know if there are more elements in the list.
-                ilst_size = atom_header.size - 8;
+                ilst_size = atom_header.sizeExcludingHeader();
 
                 state = .in_ilst;
                 continue;
@@ -220,19 +282,24 @@ pub fn read(allocator: Allocator, reader: anytype, seekable_stream: anytype) !Me
                 }
 
                 if (getMetadataAtom(&atom_header.name)) |atom| {
-                    const data_atom = try readDataAtom(allocator, reader, seekable_stream);
-                    defer allocator.free(data_atom.value);
+                    const data_atom = try DataAtom.read(reader, seekable_stream);
+                    const maybe_well_known_type = data_atom.indicators.getWellKnownType();
 
-                    // We only care about metadata with the well-known type 1, meaning a UTF-8 string.
+                    if (maybe_well_known_type) |well_known_type| {
+                        switch (well_known_type) {
+                            .utf8 => {
+                                var value = try data_atom.readValueAsBytes(allocator, reader);
+                                defer allocator.free(value);
 
-                    if (data_atom.getType() != 0) {
-                        return error.InvalidDataAtomType;
+                                try metadata.map.put(atom.export_name, value);
+                            },
+                            else => {
+                                try data_atom.skipValue(seekable_stream);
+                            },
+                        }
+                    } else {
+                        try data_atom.skipValue(seekable_stream);
                     }
-                    if (data_atom.getWellKnownType() != 1) {
-                        return error.InvalidDataAtomWellKnownType;
-                    }
-
-                    try metadata.map.put(atom.export_name, data_atom.value);
 
                     continue;
                 }
@@ -240,8 +307,7 @@ pub fn read(allocator: Allocator, reader: anytype, seekable_stream: anytype) !Me
         }
 
         // Skip every atom we don't recognize or are not interested in.
-
-        try seekable_stream.seekBy(atom_header.size - 8);
+        try seekable_stream.seekBy(atom_header.sizeExcludingHeader());
     }
 
     return metadata;
@@ -264,17 +330,23 @@ test "atom size too big" {
 }
 
 test "data atom bad type" {
-    // 0xAB is not a valid data atom type
-    const data = "\x00\x00\x00\x08moov\x00\x00\x00\x08udta\x00\x00\x00\x08meta\x01\x00\x00\x00\x00\x00\x00\x08ilst\x00\x00\x00\x08aART\x00\x00\x00\x10data\xAB\x00\x00\x00\x00\x00\x00\x00";
-    const res = readData(std.testing.allocator, data);
-    try std.testing.expectError(error.InvalidDataAtomType, res);
+    // 0xAB is not a valid data atom type, it should be skipped
+    const data = "\x00\x00\x00\x08moov\x00\x00\x00\x08udta\x00\x00\x00\x08meta\x01\x00\x00\x00\x00\x00\x00\x30ilst\x00\x00\x00\x08aART\x00\x00\x00\x10data\xAB\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\xA9nam\x00\x00\x00\x10data\x00\x00\x00\x01\x00\x00\x00\x00";
+    var metadata = try readData(std.testing.allocator, data);
+    defer metadata.deinit();
+
+    // the invalid atom should be skipped but the valid one should be read
+    try std.testing.expectEqual(@as(usize, 1), metadata.map.entries.items.len);
 }
 
 test "data atom bad well-known type" {
-    // 0xFFFFFF is not a valid data atom well-known type
-    const data = "\x00\x00\x00\x08moov\x00\x00\x00\x08udta\x00\x00\x00\x08meta\x01\x00\x00\x00\x00\x00\x00\x08ilst\x00\x00\x00\x08aART\x00\x00\x00\x10data\x00\xFF\xFF\xFF\x00\x00\x00\x00";
-    const res = readData(std.testing.allocator, data);
-    try std.testing.expectError(error.InvalidDataAtomWellKnownType, res);
+    // 0xFFFFFF is not a valid data atom well-known type, it should be skipped
+    const data = "\x00\x00\x00\x08moov\x00\x00\x00\x08udta\x00\x00\x00\x08meta\x01\x00\x00\x00\x00\x00\x00\x30ilst\x00\x00\x00\x08aART\x00\x00\x00\x10data\x00\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x08\xA9nam\x00\x00\x00\x10data\x00\x00\x00\x01\x00\x00\x00\x00";
+    var metadata = try readData(std.testing.allocator, data);
+    defer metadata.deinit();
+
+    // the invalid atom should be skipped but the valid one should be read
+    try std.testing.expectEqual(@as(usize, 1), metadata.map.entries.items.len);
 }
 
 test "unimplemented extended size" {
@@ -282,7 +354,7 @@ test "unimplemented extended size" {
     try std.testing.expectError(error.UnimplementedExtendedSize, res);
 }
 
-fn readData(allocator: Allocator, data: []const u8) !void {
+fn readData(allocator: Allocator, data: []const u8) !Metadata {
     var stream_source = std.io.StreamSource{ .const_buffer = std.io.fixedBufferStream(data) };
-    _ = try read(allocator, stream_source.reader(), stream_source.seekableStream());
+    return try read(allocator, stream_source.reader(), stream_source.seekableStream());
 }
