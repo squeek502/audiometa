squeek502/audiometa

commit 5d0a61d9490a98d10d7090dd7bd910f49fa5fca2

Bump default Windows-1251 detection threshold to min 4 cyrillic letters
Was hitting a false positive with the band name ØØØ. Some more sophisticated detection might be worth investigating in the future (looking at tags as a whole and seeing if multiple fields are detected or something like that)

src/windows1251.zig
@@ -122,7 +122,7 @@ pub fn Detector(comptime threshold: Windows1251DetectionThreshold) type {
     };
 }
 
-pub const DefaultDetector = Detector(.{ .streak = 4, .min_cyrillic_letters = 2 });
+pub const DefaultDetector = Detector(.{ .streak = 4, .min_cyrillic_letters = 4 });
 
 pub fn couldBeWindows1251(text: []const u8) bool {
     var detector = DefaultDetector{};

commit d08ae234eea62d1894ff9e125f6794543a095129

Fix windows1251 test regression caused by 5d0a61d9490a98d10d7090dd7bd910f49fa5fca2

src/windows1251.zig
@@ -173,7 +173,7 @@ test "could be windows1251" {
     try std.testing.expect(!couldBeWindows1251("abc\xC8\xC8"));
     try std.testing.expect(!couldBeWindows1251("\xC8\xC8\xC8\xC8\x98"));
     try std.testing.expect(!couldBeWindows1251("a\xE0b\xE6c\xEFd"));
-    try std.testing.expect(couldBeWindows1251("\xC8\xC8"));
+    try std.testing.expect(couldBeWindows1251("\xC8\xC8\xC8\xC8"));
     try std.testing.expect(couldBeWindows1251("abc\xC8\xC8\xE6\xEF"));
 }
 